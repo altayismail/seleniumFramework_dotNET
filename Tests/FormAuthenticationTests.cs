f8131f7f552e5a424fb18b4330fcf544c20377d3
İsmail Altay <ismailaltay3553@gmail.com>
2022-09-17 17:37:11 +0300

    Attribute for test cases added. Attributes are giving category to a case, to run case in parallel with the other case

--- Tests/FormAuthenticationTests.cs
+++ Tests/FormAuthenticationTests.cs
@@ -4,8 +4,9 @@ namespace Tests
 {
     public class FormAuthenticationTests : BaseTestClass
     {
-        [Test]
+        [Test, Category("Form Authentication")]
         [TestCase("tomsmith", "SuperSecretPassword!")]
+        [Parallelizable(ParallelScope.Children)]
         public void SuccessfulLogin(string username, string password)
         {
             FormAuthenticationPage page = homePage.goToFormAuthenticationPage();
@@ -15,9 +16,10 @@ namespace Tests
             Assert.AreEqual("You logged into a secure area!\r\n×", page.getSuccessfulLoginAlert());
         }
 
-        [Test]
+        [Test, Category("Form Authentication")]
         [TestCase("tomsmithh", "SuperSecretPassword!")]
         [TestCase("", "SuperSecretPassword!")]
+        [Parallelizable(ParallelScope.Children)]
         public void InvalidUsername(string username, string password)
         {
             FormAuthenticationPage page = homePage.goToFormAuthenticationPage();
@@ -27,8 +29,9 @@ namespace Tests
             Assert.AreEqual("Your username is invalid!\r\n×", page.getInvalidUsernameAlert());
         }
 
-        [Test]
+        [Test, Category("Form Authentication")]
         [TestCase("tomsmith", "SuperSecretPassword")]
+        [Parallelizable(ParallelScope.Children)]
         public void InvalidPassword(string username, string password)
         {
             FormAuthenticationPage page = homePage.goToFormAuthenticationPage();
